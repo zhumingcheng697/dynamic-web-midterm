96e72a6dc895ef3bfda9889d72ad8cf3bfcd8664
McCoy Zhu <mz1945@nyu.edu>
2020-10-21 23:50:28 -0700

    Updates

---
+++ precache-manifest.634ecfa7f207d74cc52fa7d0b391d082.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "275c1356f659e2b3cd8e74a492af8280",
+    "url": "/dynamic-web-midterm/index.html"
+  },
+  {
+    "revision": "f8e2ba923c4bbd9df9e1",
+    "url": "/dynamic-web-midterm/static/css/main.b9b85895.chunk.css"
+  },
+  {
+    "revision": "cbde26154dfd2781add3",
+    "url": "/dynamic-web-midterm/static/js/2.d2a6c166.chunk.js"
+  },
+  {
+    "revision": "e4ffd193b4f6a408e4148eb51fbf76ac",
+    "url": "/dynamic-web-midterm/static/js/2.d2a6c166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8e2ba923c4bbd9df9e1",
+    "url": "/dynamic-web-midterm/static/js/main.3b0fc4d0.chunk.js"
+  },
+  {
+    "revision": "564ae71983fb74e1c037",
+    "url": "/dynamic-web-midterm/static/js/runtime-main.0790086b.js"
+  }
+]);
